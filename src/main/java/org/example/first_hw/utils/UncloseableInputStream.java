47a800c7c8e4fcf8d3e664fd49996e6d3d09e065
trifonova_o_e <myowl@list.ru>
2024-12-25 23:23:37 +0300

    2

---
+++ src/main/java/org/example/first_hw/utils/UncloseableInputStream.java
@@ -0,0 +1,21 @@
+package org.example.first_hw.utils;
+
+import java.io.FilterInputStream;
+import java.io.InputStream;
+/**
+ * Класс-обёртка InputStream, позволяющий использовать исходный поток без риска
+ * его закрытия, что может быть необходимо при использовании потока,
+ * предоставляемого и управляемого внешним сервисом.
+ */
+public final class UncloseableInputStream extends FilterInputStream {
+    private UncloseableInputStream(InputStream in) {
+        super(in);
+    }
+    @Override
+    public void close() {
+        // do nothing
+    }
+    public static InputStream wrap(InputStream inputStream) {
+        return new UncloseableInputStream(inputStream);
+    }
+}
